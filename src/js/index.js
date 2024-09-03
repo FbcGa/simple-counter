6359d608565ad2b4e02a014f39ec11d163dabb7d
FbcGa <fabricio.alipazaga.pe@gmail.com>
2024-09-03 17:16:13 -0500

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,46 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import PropTypes from "prop-types";
 
-// include your styles into the webpack bundle
+//include your index.scss file into the bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour}</div>
+      <div className="three">{props.digitThree}</div>
+      <div className="two">{props.digitTwo}</div>
+      <div className="one">{props.digitOne}</div>
+    </div>
+  );
+}
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+};
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+let counter = 0;
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+setInterval(function () {
+  const four = Math.floor(counter / 1000) % 10;
+  const three = Math.floor(counter / 100) % 10;
+  const two = Math.floor(counter / 10) % 10;
+  const one = counter % 10;
+  counter++;
+  root.render(
+    <React.StrictMode>
+      <SimpleCounter
+        digitOne={one}
+        digitTwo={two}
+        digitThree={three}
+        digitFour={four}
+      />
+    </React.StrictMode>
+  );
+}, 1000);
